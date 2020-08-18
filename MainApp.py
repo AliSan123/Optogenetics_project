bccce5352c7486ef37a7e2c0a8631bc9b2549251
AliSan123 <alisonsanders.11@gmail.com>
2020-08-18 01:35:55 +0100

    MainApp Frontend
    Updated MainApp up to Experiments tab. Still need to do data analysis and Experiments backend functionality

--- MainApp.py
+++ MainApp.py
@@ -1,19 +1,23 @@
+arduino.close_port()
+coherent.close_port()
+
+import os
+os.chdir(r'C:\Users\user\Desktop\2019 - MSc\Project\Scripts\Optogenetics_project')
+
 from PyQt5 import QtWidgets, uic, QtGui
 from PyQt5.QtWidgets import QDialog, QFileDialog
 from icons_rc import *
 import sys
 import webbrowser
-import os
 import datetime
 import pandas as pd
 import numpy as np
+import shutil
 import Coherent
 import Arduino
 import functions as fn
 
 
-
-os.chdir(r'C:\Users\user\Desktop\2019 - MSc\Project\Scripts\Optogenetics_project')
 
 SafetyWindow_ui,_=uic.loadUiType('SafetyWindow.ui')
 UploadPowCalResults_ui,_=uic.loadUiType('UploadPowCalResults.ui')
@@ -46,9 +50,14 @@ class Ui(QtWidgets.QMainWindow):
 
 #       POWER CALIBRATION TAB    
         
-        self.setParams.clicked.connect(self.getParamVals)        
-        self.RunButton.clicked.connect(self.OpenSafetyWindow)
+        self.setParams.clicked.connect(self.getParamVals) 
+        self.setParams.clicked.connect(self.RunButtonToGreen)
+        self.RunButton.clicked.connect(lambda: self.OpenSafetyWindow('Power'))
 
+#       MRR CALIBRATION TAB 
+        self.setMRRParams.clicked.connect(self.getMRRParamVals) 
+        self.setMRRParams.clicked.connect(self.RunButtonToGreen2)
+        self.RunButton_2.clicked.connect(lambda: self.OpenSafetyWindow('MRR'))
         
 #       HOME SCREEN  
     def OpenTermsOfUse(self):
@@ -103,7 +112,10 @@ class Ui(QtWidgets.QMainWindow):
         DailyDirectory=self.lineEditDirectory_3.text()
         os.chdir(DailyDirectory)
         self.set3.setStyleSheet("font: 8pt \"Eras Bold ITC\"; color: rgb(0, 170, 0)")
-        return DailyDirectory    
+        
+    def getDailyDirectory(self):
+        DailyDirectory=self.lineEditDirectory_3.text()
+        return DailyDirectory
     
     def clickedCreateTimeFolder(self):
         self.createFolder(datetime.datetime.now().strftime('%Y-%m-%d_%Hh%Mm%S'))
@@ -122,26 +134,28 @@ class Ui(QtWidgets.QMainWindow):
     
     def UploadCalibrationFile(self):
         calibration_fname,_filter=QFileDialog.getOpenFileName(self, 'Upload File')
-        input_file=np.loadtxt(str(calibration_fname))
-        output_df=pd.DataFrame(input_file)
-        self.SetDailyDirectory() #change directory to saving location
-        Filedirectory='power_calibration_980nm_8um_spot -' + datetime.datetime.now().strftime('%Y-%m-%d') + '.dat'
-        output_df.to_csv(Filedirectory)
-        self.lineEditFile.setText(os.getcwd() + '\\' + Filedirectory)
-        
+        newPath=shutil.copy(calibration_fname,self.getDailyDirectory() + '\\' + 'power_calibration_980nm_8um_spot -' + datetime.datetime.now().strftime('%Hh%Mm%Ss') + '.dat')
+        self.lineEditFile.setText(newPath)      
         self.NextButton.setStyleSheet("font: 14pt \"Eras Bold ITC\"; color: rgb(0, 170, 0)")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/Icons/QTIcons/RunArrow.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.NextButton.setIcon(icon)
+        return newPath       
+
 
     def GoToPowerCalibration(self):
         self.tabWidget.setCurrentIndex(2)
 
 #       POWER CALIBRATION
-    def OpenSafetyWindow(self): 
-        MRR_in_kHz,PW_in_fs,RRDivisor,PulsesPerMBurst,energy_as_frac,pulse_duration_ms,n_times,steps=self.getParamVals()
+    def OpenSafetyWindow(self,Type): 
+        steps=0 #giving it a value because not applicable for 'MRR'
+        if Type=='Power':
+            MRR_in_kHz,PW_in_fs,RRDivisor,PulsesPerMBurst,energy_as_frac,pulse_duration_ms,n_times,steps=self.getParamVals()
+        elif Type=='MRR':
+            MRR_in_kHz,PW_in_fs,RRDivisor,PulsesPerMBurst,energy_as_frac,pulse_duration_ms,n_times=self.getMRRParamVals()
+            
         TimeDirectory=self.getTimeDirectory()
-        SW=SafetyWindow(self.arduino,self.coherent,TimeDirectory,MRR_in_kHz,PW_in_fs,RRDivisor,PulsesPerMBurst,energy_as_frac,pulse_duration_ms,n_times,steps)
+        SW=SafetyWindow(self.arduino,self.coherent,Type,TimeDirectory,MRR_in_kHz,PW_in_fs,RRDivisor,PulsesPerMBurst,energy_as_frac,pulse_duration_ms,n_times,steps)
         SW.exec_()
         
     def getParamVals(self):
@@ -153,9 +167,9 @@ class Ui(QtWidgets.QMainWindow):
         RRDivisor=combo_lst[2]
         PulsesPerMBurst=combo_lst[3]
         # energy params
-        min_energy_as_frac=self.energy_spinbox.value()
+        min_energy_as_frac=self.energy_spinbox.value()/100
         print(min_energy_as_frac)
-        max_energy_as_frac=self.energy_spinbox_2.value()
+        max_energy_as_frac=self.energy_spinbox_2.value()/100
         print(max_energy_as_frac)
         steps=self.energy_spinbox_3.value()
         delta_energy=(max_energy_as_frac-min_energy_as_frac)/steps #"delta" -change in energy with each ramp up
@@ -165,16 +179,46 @@ class Ui(QtWidgets.QMainWindow):
         print(pulse_duration_ms)
         return MRR_in_kHz,PW_in_fs,RRDivisor,PulsesPerMBurst,delta_energy,pulse_duration_ms,n_times,steps
               
+    def RunButtonToGreen(self):
+        self.RunButton.setStyleSheet("font: 14pt \"Eras Bold ITC\"; color: rgb(0, 170, 0)")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/Icons/QTIcons/RunArrow.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.RunButton.setIcon(icon)
         
-        
+# MRR Calibration
+    def getMRRParamVals(self):
+        #Repetition rate parameters   
+        combo_txt=self.MRRcomboBox.currentText()
+        combo_lst=combo_txt.split(",") 
+        MRR_in_kHz=combo_lst
+        print(MRR_in_kHz)
+        PW_in_fs=self.PWdoubleSpinBox.value()
+        RRDivisor=1
+        PulsesPerMBurst=1
+        # energy params
+        energy_as_frac=self.energy_spinbox_5.value()/100
+        n_times=self.MRR_spinbox.value()
+        print(n_times)
+        pulse_duration_ms=self.CalTimeSpinBox_2.value()        
+        print(pulse_duration_ms)
+        return MRR_in_kHz,PW_in_fs,RRDivisor,PulsesPerMBurst,energy_as_frac,pulse_duration_ms,n_times
+    
+    def RunButtonToGreen2(self):
+        self.RunButton_2.setStyleSheet("font: 14pt \"Eras Bold ITC\"; color: rgb(0, 170, 0)")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/Icons/QTIcons/RunArrow.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.RunButton_2.setIcon(icon)
+                    
+
+
         
 class SafetyWindow(QDialog,SafetyWindow_ui):
-    def __init__(self,arduino,coherent,TimeDirectory,MRR_in_kHz,PW_in_fs,RRDivisor,PulsesPerMBurst,energy_as_frac,pulse_duration_ms,n_times,steps):
+    def __init__(self,arduino,coherent,Type,TimeDirectory,MRR_in_kHz,PW_in_fs,RRDivisor,PulsesPerMBurst,energy_as_frac,pulse_duration_ms,n_times,steps=0):
         QDialog.__init__(self)
         SafetyWindow_ui.__init__(self)
         self.setupUi(self)
         
-        
+        self.Type=Type
         self.arduino=arduino
         self.coherent=coherent
         self.TimeDirectory=TimeDirectory
@@ -205,64 +249,64 @@ class SafetyWindow(QDialog,SafetyWindow_ui):
                   
     def StartLaserCalibration(self):
         self.coherent.startup()
-        self.coherent.set_MRR(self.MRR_in_kHz,self.PW_in_fs,self.RRDivisor,self.PulsesPerMBurst)
-        for i in range(self.steps):
-            print(self.energy_as_frac*(i+1))
-            self.coherent.set_energy(self.energy_as_frac*(i+1))
-            self.coherent.start_lasing()               
-            self.arduino.TTL_sequence(self.pulse_duration_ms, self.n_times, min_time_off=0)
-        self.coherent.stop_lasing()
-        print('Completed lasing')
-        self.close() # close window
-        #Open new window for results
-        NewWindow=UploadPowCalResults(self.TimeDirectory)
+        if self.Type=='Power':
+            self.coherent.set_MRR(self.MRR_in_kHz,self.PW_in_fs,self.RRDivisor,self.PulsesPerMBurst)
+            for i in range(self.steps):
+                print(self.energy_as_frac*(i+1))
+                self.coherent.set_energy(self.energy_as_frac*(i+1))
+                self.coherent.start_lasing()               
+                self.arduino.TTL_sequence(self.pulse_duration_ms, self.n_times, min_time_off=0)
+            self.coherent.stop_lasing()
+            print('Completed lasing')
+            self.close() # close window
+            #Open new window for results
+            NewWindow=UploadResults(self.TimeDirectory,self.Type)
+        elif self.Type=='MRR':
+            self.coherent.startup()
+            self.coherent.set_energy(self.energy_as_frac)
+            for i in range(len(self.MRR_in_kHz)):
+                print(len(self.MRR_in_kHz))
+                print(self.MRR_in_kHz[i])
+                self.coherent.set_MRR(self.MRR_in_kHz,self.PW_in_fs,self.RRDivisor,self.PulsesPerMBurst)
+                self.coherent.start_lasing()               
+                self.arduino.TTL_sequence(self.pulse_duration_ms, self.n_times, min_time_off=0)       
+            self.coherent.stop_lasing()
+            print('Completed lasing')
+            self.close() # close window
+            #Open new window for results
+            NewWindow=UploadResults(self.TimeDirectory,self.Type)
         NewWindow.exec_()
         
         
 # After laser calibration run
-class UploadPowCalResults(QDialog,UploadPowCalResults_ui):
-    def __init__(self,TimeDirectory):
+class UploadResults(QDialog,UploadPowCalResults_ui):
+    def __init__(self,TimeDirectory,Type):
         QDialog.__init__(self)
         UploadPowCalResults_ui.__init__(self)
         self.setupUi(self)
        
-        self.TimeDirectory=TimeDirectory       
-        self.BrowseButtonPC.clicked.connect(self.UploadPowCalResults)
+        self.TimeDirectory=TimeDirectory 
+        self.Type=Type
+        
+        self.BrowseButtonPC.clicked.connect(self.Upload)
         self.AnalyseButton.clicked.connect(self.analyseCalData)
         
         
-    def UploadPowCalResults(self):
+    def Upload(self):
         results_fname,_filter1=QFileDialog.getOpenFileName(self, 'Upload File')
-        input_file=fn.load_ephys(results_fname) #load file 
-        output_df=pd.DataFrame(np.squeeze(ephys.segments[0].analogsignals[0]))
-        os.chdir(self.TimeDirectory)#change directory to saving location
-        FileName='power_calibration_results -' + datetime.datetime.now().strftime('%Hh%Mm%Ss') + '.smr'
-        output_df.to_csv(FileName)
-        self.lineEditDir.setText(os.getcwd() + '\\' + FileName)
-        Filedirectory=os.getcwd() + '\\' + FileName
+        if self.Type=='Power':                       
+            newPath=shutil.copy(results_fname,self.TimeDirectory + '\\' + 'power_calibration_results -' + datetime.datetime.now().strftime('%Hh%Mm%Ss') + '.smr')            
+        elif self.Type=='MRR':
+            newPath=shutil.copy(results_fname,self.TimeDirectory + '\\' + 'MRR_calibration_results -' + datetime.datetime.now().strftime('%Hh%Mm%Ss') + '.smr')
+        
+        self.lineEditDir.setText(newPath)      
         self.AnalyseButton.setStyleSheet("font: 14pt \"Eras Bold ITC\"; color: rgb(0, 170, 0)")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/Icons/QTIcons/RunArrow.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.AnalyseButton.setIcon(icon)
-        return Filedirectory
+        return newPath
     
-    def analyseCalData(self):
-        '''
-        Filepath - Path to the .smr file that was uploaded with "UploadPowCalResults"
-            
-        '''       
-        FilePath=self.UploadPowCalResults()
-        ephys=fn.load_ephys(FilePath) #load file        
-        picker = ephys.segments[0].analogsignals[0][:,1] #this channel shows the power measurement from a beam sampler, proportional to the actual laser power in the system.
-
-        f.plot_data(Im.times,np.squeeze(Im),None,f'Membrane Current\n({Im.units})','Membrane current vs time',[-1,1],411,show=True)
-        f.plot_data(Vm.times,np.squeeze(Vm),None,f'Membrane Voltage\n({Vm.units})','Membrane voltage vs time',[-50,-10],412,show=True)
-        f.plot_data(picker.times,np.squeeze(picker),'Time (s)','Picker power meter\nmeasurement output (V)',\
-            'Picker power meter (measurement output) voltage vs time',None,414)
-
-
-        #plot the figure, save to png in folder, display png?
-        
+    def analyseCalData(self):      
         print('Not written yet.')
         
         
